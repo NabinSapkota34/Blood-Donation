9a0a8a68f9fbf3c275f7e616a5e9fdab3f6d5d51
Nabin Sapkota <nawinssap123@gmail.com>
2022-12-31 16:12:34 +0545

    added admin template

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\CustomAuthController;
 use App\Http\Controllers\ForgotPasswordController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\Admin\IndexController;
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,9 +19,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-})->middleware('auth');
+Route::get('/',[IndexController::class,'index'])->name('index')->middleware(['auth']);
+
 
 Route::get('login', [CustomAuthController::class, 'index'])->name('login');
 Route::get('register', [CustomAuthController::class, 'register'])->name('register');
